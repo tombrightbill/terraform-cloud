722061164e4faa44dae0013883961ddb28a6c2a5
Tom Brightbill <tbrightbill@cloudflare.com>
2019-12-03 18:13:46 -0800

    added key as ENV variable

--- provider.tf
+++ provider.tf
@@ -1,10 +1,10 @@
-variable "path" {
-    default = "/Users/CloudFlare/Downloads"
- }
+# variable "path" {
+#     default = "/Users/CloudFlare/Downloads"
+#  }
 
 provider "google" {
 	project = "globalse-198312"
 	region = "us-west1"
 	zone = "us-west1-c"
-	credentials = var.gcp_key
+	#credentials = file("${var.path}/globalse-198312-587b99fa7f57.json")
 }
